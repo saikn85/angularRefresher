12eeee489074ebfe1136bbdc0c0d8de2f443b526
Mad Max <85madmaxfury@gmail.com>
2023-02-01 23:53:32 +0530

    day 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
 
-import { AppComponent } from './app.component';
-import { ProductPageComponent } from './product-page/product-page.component';
+import { AppComponent } from "./app.component";
+import { ProductPageComponent } from "./product-page/product-page.component";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ProductPageComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
+  declarations: [AppComponent, ProductPageComponent],
+  imports: [BrowserModule, FormsModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
