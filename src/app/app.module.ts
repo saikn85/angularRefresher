e6f61f99b976c92229ba4eb6cf2d704ad2e3b678
Mad Max <85madmaxfury@gmail.com>
2023-02-01 23:57:22 +0530

    day 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { HttpClientModule } from "@angular/common/http";
 
 import { AppComponent } from "./app.component";
 import { ProductPageComponent } from "./product-page/product-page.component";
+import { ProductDescriptionComponent } from './product-description/product-description.component';
 
 @NgModule({
-  declarations: [AppComponent, ProductPageComponent],
+  declarations: [AppComponent, ProductPageComponent, ProductDescriptionComponent],
   imports: [BrowserModule, FormsModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
