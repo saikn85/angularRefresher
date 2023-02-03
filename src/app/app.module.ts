58c3490d18b98fe8eb1c6c9e419d200ce6433692
Mad Max <85madmaxfury@gmail.com>
2023-02-03 23:31:06 +0530

    day 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,16 @@ import { HttpClientModule } from "@angular/common/http";
 import { AppComponent } from "./app.component";
 import { ProductPageComponent } from "./product-page/product-page.component";
 import { ProductDescriptionComponent } from './product-description/product-description.component';
+import { ProductService } from "./product.service";
 
 @NgModule({
-  declarations: [AppComponent, ProductPageComponent, ProductDescriptionComponent],
-  imports: [BrowserModule, FormsModule, HttpClientModule],
-  providers: [],
-  bootstrap: [AppComponent],
+    declarations: [
+        AppComponent,
+        ProductPageComponent,
+        ProductDescriptionComponent,
+    ],
+    imports: [BrowserModule, FormsModule, HttpClientModule],
+    providers: [ProductService],
+    bootstrap: [AppComponent],
 })
 export class AppModule {}
---
+++ src/app/product.service.ts
@@ -0,0 +1,15 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { map } from "rxjs";
+
+@Injectable({
+    providedIn: "root",
+})
+export class ProductService {
+    private _albumUrl: string = "../assets/album.json";
+    constructor(private _http: HttpClient) {}
+
+    getAlbum(id: number) {
+        return this._http.get(this._albumUrl).map();
+    }
+}
